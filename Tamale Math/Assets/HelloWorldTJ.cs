99af3fd8fb6df6d131d63371372b5817a248ac81
TobiasJensen08 <tobjensen1@gmail.com>
2021-09-29 17:29:27 -0600

    Added Hello World - TJ

---
+++ Tamale Math/Assets/HelloWorldTJ.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldTJ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World. -- From Tobias Jensen");
+    }
+}
