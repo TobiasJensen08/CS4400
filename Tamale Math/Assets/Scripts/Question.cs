2ba6f25a1d0113c1be9b49f3f6fd2dd1a593d95d
Chloe Howling <chloehltam@gmail.com>
2022-02-03 16:42:11 -0700

    UI scale with screen size

--- Tamale Math/Assets/Scripts/Question.cs
+++ Tamale Math/Assets/Scripts/Question.cs
@@ -5,9 +5,11 @@ public class Questions
 {
     public string content;
     // the correct answer is always the first item in the list
-    public string choices[];
+    public string[] choices;
 }
 
 public class QuestionManager
 {
+
+
 }
