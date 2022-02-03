bdf441bdd25453ce127da4f9fe1f71c260dd1380
Chloe Howling <chloehltam@gmail.com>
2022-02-03 15:48:16 -0700

    UI tweak

---
+++ Tamale Math/Assets/Scripts/Question.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Questions
+{
+    public string content;
+    // the correct answer is always the first item in the list
+    public string choices[];
+}
+
+public class QuestionManager
+{
+}
