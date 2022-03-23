1f395a20e68eedc6ad265f345b5b6ddf5913f615
romigrolli <74804606+romigrolli@users.noreply.github.com>
2022-03-23 04:56:13 -0600

    CountDownTimer

---
+++ Tamale Math/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CountDownTimer : MonoBehaviour
+{
+   float currentTime = 0f;
+   float startingTime = 10f;
+   [SerializeField] Text countdowntimerText;
+
+   void Start()
+   {
+       currentTime = startingTime;
+   }
+
+   void Update()
+   {
+       currentTime -= 1 * Time.deltaTime;
+       countdowntimerText.text = currentTime.ToString();
+   }
+
+   
+}
